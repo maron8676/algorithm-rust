8ef0481a3f70499d6986cd010b84e21bb1a741af
maron8676 <maron8676@gmail.com>
2020-06-20 03:01:02 +0900

    extract basic function

--- src/algorithm/basic.rs
+++ src/algorithm/basic.rs
@@ -1,5 +1,31 @@
+fn max<T>(v: Vec<T>) -> Option<T>
+where
+    T: Ord + Copy,
+{
+    v.iter().copied().max()
+}
+
+fn arg_max<T>(v: Vec<T>) -> Option<usize>
+where
+    T: Ord + Copy,
+{
+    v.iter()
+        .enumerate()
+        .max_by_key(|&(_index, value)| value)
+        .map_or(None, |(index, _)| Some(index))
+}
+
+fn sum<T>(v: Vec<T>) -> T
+where
+    T: std::iter::Sum + Copy,
+{
+    v.iter().copied().sum()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn swap_test() {
         let mut a = 1;
@@ -14,34 +40,31 @@ mod tests {
         assert_eq!(2, std::cmp::max(1, 2));
         assert_eq!(2, std::cmp::max(2, 2));
         assert_eq!(3, std::cmp::max(2, 3));
+
+        assert_eq!(Some(4), max(vec![2, 4, 3, 1]));
+        assert_eq!(None as Option<i32>, max(vec![]));
+
+        assert_eq!(Some(1), arg_max(vec![2, 4, 3, 1]));
+        assert_eq!(None as Option<usize>, arg_max(vec![] as Vec<i32>));
     }
 
     #[test]
     fn sum_test() {
-        assert_eq!(10, vec![1, 2, 3, 4].iter().sum());
+        assert_eq!(10, sum(vec![1, 2, 3, 4]));
 
-        assert_eq!(
-            Some(10),
-            vec![Some(1), Some(2), Some(3), Some(4)]
-                .iter()
-                .copied()
-                .sum()
-        );
+        assert_eq!(Some(10), sum(vec![Some(1), Some(2), Some(3), Some(4)]));
         assert_eq!(
             None as Option<i32>,
-            vec![Some(1), Some(2), None, Some(4)].iter().copied().sum()
+            sum(vec![Some(1), Some(2), None, Some(4)])
         );
-        
+
         assert_eq!(
             Ok(10) as Result<i32, &str>,
-            vec![Ok(1), Ok(2), Ok(3), Ok(4)]
-                .iter()
-                .copied()
-                .sum()
+            sum(vec![Ok(1), Ok(2), Ok(3), Ok(4)])
         );
         assert_eq!(
             Err("error") as Result<i32, &str>,
-            vec![Ok(1), Ok(2), Err("error"), Ok(4)].iter().copied().sum()
+            sum(vec![Ok(1), Ok(2), Err("error"), Ok(4)])
         );
     }
 }
