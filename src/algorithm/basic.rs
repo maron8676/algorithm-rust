fab70732684477583aa73b39504fc973f75d59c0
maron8676 <maron8676@gmail.com>
2020-06-20 03:06:13 +0900

    add min function

--- src/algorithm/basic.rs
+++ src/algorithm/basic.rs
@@ -15,6 +15,23 @@ where
         .map_or(None, |(index, _)| Some(index))
 }
 
+fn min<T>(v: Vec<T>) -> Option<T>
+where
+    T: Ord + Copy,
+{
+    v.iter().copied().min()
+}
+
+fn arg_min<T>(v: Vec<T>) -> Option<usize>
+where
+    T: Ord + Copy,
+{
+    v.iter()
+        .enumerate()
+        .min_by_key(|&(_index, value)| value)
+        .map_or(None, |(index, _)| Some(index))
+}
+
 fn sum<T>(v: Vec<T>) -> T
 where
     T: std::iter::Sum + Copy,
@@ -46,6 +63,19 @@ mod tests {
 
         assert_eq!(Some(1), arg_max(vec![2, 4, 3, 1]));
         assert_eq!(None as Option<usize>, arg_max(vec![] as Vec<i32>));
+    }
+
+    #[test]
+    fn min_test(){
+        assert_eq!(1, std::cmp::min(1, 2));
+        assert_eq!(2, std::cmp::min(2, 2));
+        assert_eq!(2, std::cmp::min(2, 3));
+
+        assert_eq!(Some(1), min(vec![2, 4, 3, 1]));
+        assert_eq!(None as Option<i32>, min(vec![]));
+
+        assert_eq!(Some(3), arg_min(vec![2, 4, 3, 1]));
+        assert_eq!(None as Option<usize>, arg_min(vec![] as Vec<i32>));
     }
 
     #[test]
